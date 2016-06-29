ce530d0bd078e471c579b09e90249630ebab4b46
Christopher Cinco <christopher@Tope-MBP.local.gap>
2016-06-29 17:34:52 +0800

    chunked fs collection into smaller pieces to optimize uploading

--- build/fitstorm/both/collections/songs.js
+++ build/fitstorm/both/collections/songs.js
@@ -1,5 +1,7 @@
 this.Songs = new FS.Collection("songs", {
-	stores: [new FS.Store.GridFS("songs", {})]
+	stores: [new FS.Store.GridFS("songs", {})],
+	chunkSize	 : 1024*1024,
+	mongoOptions : { db: { native_parser: false }, server: { auto_reconnect: true }},
 });
 
 this.Songs.userCanInsert = function(userId, doc) {
